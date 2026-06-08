aa48b83c2d8ef452d1adbb8583f13aa6b5a92f26
nghtrcom-art <nghtr.com@gmail.com>
2026-06-08 17:04:09 -0300

    atividade pronta

--- src/condicionais.java
+++ src/condicionais.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class condicionais {
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite dois números para compararmos:");
+        int num1 = sc.nextInt();
+        int num2 =sc.nextInt();
+
+        if(num1 > num2){
+
+            System.out.println("O número " + num1 + " é o maior dos dois números");
+
+        }else if(num2 > num1){
+
+            System.out.println("O número " + num2 + " é o maior dos dois números");
+
+        } else if (num1 == num2) {
+
+            System.out.println("Os dois números são iguais");
+
+        }else{
+
+            System.out.println("Opção inválida");
+        }
 
 
     }
